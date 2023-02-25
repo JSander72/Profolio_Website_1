4a51d52dd691906e7c9c6db9e8941da1c3ad61e2
JSander72 <jamesearlsanders2@outlook.com>
2023-02-25 17:10:51 -0500

    scoll commit

--- img.js
+++
@@ -1,9 +0,0 @@
-const image = document.getElementById("fullscreen-profile-picture");
-
-image.addEventListener("click", function() {
-  if (image.style.display === "none") {
-    image.style.display = "block";
-  } else {
-    image.style.display = "none";
-  }
-});
