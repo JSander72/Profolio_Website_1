1b5cd937d1d7c803986e1d1f96071923819956c0
JSander72 <jamesearlsanders2@outlook.com>
2024-09-06 23:35:05 -0400

    CSS imporovements

--- event_listener.js
+++ event_listener.js
@@ -1,9 +1,12 @@
 window.addEventListener('scroll', () => {
-    const navBar = document.querySelector('.nav_bar');
-    if (window.scrollY > 0) {
+  const navBar = document.querySelector('.nav_bar');
+  const navbarBrand = document.querySelector('.navbar-brand'); 
+
+  if (window.scrollY > 0) {
       navBar.classList.add('scrolling');
-    } else {
+      navbarBrand.classList.add('scrolling'); // Add the scrolling class to navbar-brand
+  } else {
       navBar.classList.remove('scrolling');
-    }
-  });
-  
+      navbarBrand.classList.remove('scrolling'); // Remove the scrolling class when at the top
+  }
+});
